785b23c04f7bf12b1e0470f6cecc0ae429f8e254
balaji <balajier2006@gmail.com>
2025-08-18 10:16:31 +0530

    changed credentials

--- server/db.js
+++ server/db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
     host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
-    port:process.env.DB_HOST
 });
 
 db.connect((err)=>{

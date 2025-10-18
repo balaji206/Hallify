c5c1b389b54e23d61d529d7a6a283e38a1c8629b
balaji <balajier2006@gmail.com>
2025-10-18 12:40:45 +0530

    frontend ready

--- server/routes/mahalroutes.js
+++ server/routes/mahalroutes.js
@@ -11,19 +11,14 @@ const {
   getMahalsbyId
 } = require('../controllers/mahalcontroller');
 
-// 🔓 Public - Anyone can fetch mahals
 router.get('/get', getMahals);
 
-// Get details of a mahal by its ID
 router.get('/get/:id', getMahalsbyId);
 
-// 🔒 Admin and Owner can add mahals (with image upload)
 router.post('/add', verifyToken(['admin', 'owner']), upload.single('image'), addMahal);
 
-// 🔒 Admin and Owner can update mahals (with image upload)
 router.put('/update/:id', verifyToken(['admin', 'owner']), upload.single('image'), updateMahal);
 
-// 🔒 Admin and Owner can delete mahals
 router.delete('/delete/:id', verifyToken(['admin', 'owner']), deleteMahal);
 
 module.exports = router;

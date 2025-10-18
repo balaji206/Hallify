804bb3c3e0ade2357b011814ad9d37acdff2a341
balaji <balajier2006@gmail.com>
2025-10-18 13:35:49 +0530

    finish

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-        // Force JS-only build for LightningCSS
-        require('lightningcss').transform
-      ]
-    }
-  }
+  plugins: [react(),tailwindcss()],
+  
 });
